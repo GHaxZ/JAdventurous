d8fbf5603341332052e19374b56b3ba3fe622887
GHaxZ <ghax.z.yt@gmail.com>
2023-03-20 17:31:27 +0100

    Second commit, added enemy class

--- src/Game.java
+++ src/Game.java
@@ -1,6 +1,4 @@
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class Game {
     private static Level homeLevel = new Level("home", new String[]{}, new String[]{});
